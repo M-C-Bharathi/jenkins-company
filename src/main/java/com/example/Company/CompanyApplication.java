bfd3992fdbbc7310f9bce9ff6b62fbb04bb5bc21
keerthanapranesh <keerthanavenkat2202@gmail.com>
2024-04-25 17:45:25 +0530

    Update CompanyApplication.java
    testing commit message

--- src/main/java/com/example/Company/CompanyApplication.java
+++ src/main/java/com/example/Company/CompanyApplication.java
@@ -8,6 +8,7 @@ public class CompanyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CompanyApplication.class, args);
+		system.out.println("commit message");
 	}
 
 }
